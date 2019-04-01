57d6b662dbc8e77c6287a34f49371c3d02875461
wlbf <lbf1353@live.com>
2019-04-01 23:06:57 +0800

    exercise 11

--- kern/monitor.c
+++ kern/monitor.c
@@ -57,7 +57,14 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-	// Your code here.
+	uint32_t* ebp;
+	ebp = (uint32_t*)read_ebp();
+
+	while (ebp != 0x0) {
+		cprintf("ebp %08x eip %08x args %08x %08x %08x %08x %08x\n",
+			ebp, *(ebp + 1), *(ebp + 2), *(ebp + 3), *(ebp + 4), *(ebp + 5), *(ebp + 6));
+		ebp = (uint32_t*)*ebp;
+	}
 	return 0;
 }
 
