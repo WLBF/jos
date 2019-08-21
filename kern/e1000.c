96cd708ee4b1707315a716c911dbfea3f6ca32b5
WLBF <lbf1353@live.com>
2019-08-21 23:40:28 +0800

    lab6 exercise 2-4

--- kern/e1000.c
+++ kern/e1000.c
@@ -2,3 +2,15 @@
 #include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+
+volatile uint8_t* reg;
+
+int e1000_init(struct pci_func *f) {
+	pci_func_enable(f);
+
+	reg = mmio_map_region(f->reg_base[0] & ~0xf, f->reg_size[0]);
+
+	cprintf("%08x\n", *(uint32_t *)(reg + E1000_STATUS));
+
+	return 0;
+}
